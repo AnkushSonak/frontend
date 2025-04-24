6de223ed384cf6fbefc3d6be6de6e835fae8cbc5
AnkushSonak <as0191696@gmail.com>
2025-04-24 21:53:03 +0530

    Login Form two completed  -86

--- src/features/login/LoginModal/LoginModal.tsx
+++ src/features/login/LoginModal/LoginModal.tsx
@@ -3,6 +3,8 @@ import { Modal } from "../../../components/Modal/Modal";
 import { LoginFormOne } from "../LoginForms/LoginFormOne";
 import { LoginModalTop } from "../LoginModalTop/LoginModalTop";
 import { RootState } from "../../../redux/Store";
+import React, { useState } from "react";
+import { LoginFormTwo } from "../LoginForms/LoginFormTwo";
 
 interface LoginModalProps{
     toggleModal: () => void;
@@ -12,10 +14,16 @@ export const LoginModal:React.FC<LoginModalProps> =({toggleModal}) =>{
 
     const state = useSelector((state:RootState) => state.user);
 
+    const [password, setPassword] = useState<string>('');
+
+    const handlePassword = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
     return (
         <Modal
             topContent={<LoginModalTop closeModal={toggleModal} />}
-            content={state.username ? <>Login Form 2</> : <LoginFormOne />}
+            content={state.username ? <LoginFormTwo setPassword={handlePassword} /> : <LoginFormOne />}
             bottomContent={state.username ?  <div>Login Form 2 Button</div> : <></>}
         /> 
     )
