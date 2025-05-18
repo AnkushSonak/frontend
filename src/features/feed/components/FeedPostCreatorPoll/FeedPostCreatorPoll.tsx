ceb05b797e766f5b8e8c73fd4fcda7229111f310
AnkushSonak <as0191696@gmail.com>
2025-05-18 21:10:54 +0530

    worked on to send the poll to the backend innside the post -129

--- src/features/feed/components/FeedPostCreatorPoll/FeedPostCreatorPoll.tsx
+++ src/features/feed/components/FeedPostCreatorPoll/FeedPostCreatorPoll.tsx
@@ -6,26 +6,26 @@ import './FeedPostCreatorPoll.css'
 import { generatePollDaysSelections, generatePollHoursSelections, generatePollMinutesSelections } from "../../utils/FeedUtils";
 import { AppDispatch } from "../../../../redux/Store";
 import { useDispatch } from "react-redux";
-import { removePoll, updatePoll } from "../../../../redux/Slices/PostSlice";
+import { removePoll, setPollDate, updatePoll } from "../../../../redux/Slices/PostSlice";
 
-export const FeedPostCreatorPoll:React.FC = () => {
+export const FeedPostCreatorPoll: React.FC = () => {
 
-    const dispatch:AppDispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
 
     const [labels, setLabels] = useState<string[]>(['Choice 1', 'Choice 2']);
-    const [time, setTime] = useState<{days: string, hours: string, minutes: string}>({
+    const [time, setTime] = useState<{ days: string, hours: string, minutes: string }>({
         days: "1",
         hours: "0",
-        minutes: "0" 
+        minutes: "0"
     })
 
     const addNewChoice = () => {
-        if(labels.length <= 4){
-           setLabels([...labels, `Choice ${labels.length + 1} (optional)`]);
+        if (labels.length <= 4) {
+            setLabels([...labels, `Choice ${labels.length + 1} (optional)`]);
         }
     }
 
-    const updateChoiceText = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const updateChoiceText = (e: React.ChangeEvent<HTMLInputElement>) => {
         const index = e.target.name.split(":")[1];
         dispatch(updatePoll({
             index: +index,
@@ -37,40 +37,49 @@ export const FeedPostCreatorPoll:React.FC = () => {
     const updateTime = (name: string, value: string | number | boolean) => {
         setTime({
             ...time,
-           [name]: value,
+            [name]: value,
         })
+
+        let date = JSON.parse(JSON.stringify(time));
+
+        date = {
+            ...date,
+            [name]: value
+        }
+
+        dispatch(setPollDate(`${date.days}:${date.hours}:${date.minutes}`));
     }
 
     const deletePoll = () => {
         dispatch(removePoll())
     }
 
-    useEffect(()=>{
-        
+    useEffect(() => {
+
     }, [labels.length]);
 
     return (
-        <div className="feed-post-creator-poll" style={{zIndex: 5}}>
+        <div className="feed-post-creator-poll" style={{ zIndex: 5 }}>
             <div className="feed-post-creator-poll-choices">
                 {
                     labels.map((label, index) => {
                         return (
                             <div className="feed-post-creator-poll-choice" key={label}>
                                 <div className={labels.length < 4 ? "feed-post-creator-poll-choice-wrapper-min" : "feed-post-creator-poll-choice-wrapper-max"}>
-                                    <ValidatedTextInput valid={true} name={`choice:${index}`} label={label} changeValue={updateChoiceText} attributes={{maxLength:25}} />
+                                    <ValidatedTextInput valid={true} name={`choice:${index}`} label={label} changeValue={updateChoiceText} attributes={{ maxLength: 25 }} />
                                 </div>
                                 {
                                     index === labels.length - 1 && labels.length < 4 &&
-                                        <div className="feed-post-creator-poll-choice-add">
-                                            <div className="feed-post-creator-poll-choice-add-hover" onClick={addNewChoice}>
-                                                <AddIcon sx={{color: 'rgba(29, 161, 242, .4)'}} />
-                                            </div>
+                                    <div className="feed-post-creator-poll-choice-add">
+                                        <div className="feed-post-creator-poll-choice-add-hover" onClick={addNewChoice}>
+                                            <AddIcon sx={{ color: 'rgba(29, 161, 242, .4)' }} />
                                         </div>
+                                    </div>
                                 }
                             </div>
                         )
                     })
-                    
+
                 }
             </div>
             <div className="feed-post-creator-poll-length">
